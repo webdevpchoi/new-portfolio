5a4955559fc2dbd32c992cee68876bbf27790f1e
WonkasWilly <pchoi13370@gmail.com>
2018-05-11 16:29:45 -0700

    Styled the projects page, and almost finished the contacts page

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -2,5 +2,9 @@ $(document).ready(function() {
 	console.log('JS loaded!')
 	const carousel = $('.proj-carousel');
 	
-	carousel.slick()
+	carousel.slick({
+		centerPadding: '0px',
+		centerMode: true,
+		dots: true,
+	})
 })
